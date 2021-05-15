532f93f3882f8601028860ed1e9ce218e764a061
summerlovelemon <ybj8763@gmail.com>
2021-05-15 19:46:12 +0800

    删掉推特

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -170,45 +170,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
     >
       <PageHead site={site} />
 
-      <Head>
-        <meta property='og:title' content={title} />
-        <meta property='og:site_name' content={site.name} />
 
-        <meta name='twitter:title' content={title} />
-        <meta property='twitter:domain' content={site.domain} />
-
-        {config.twitter && (
-          <meta name='twitter:creator' content={`@${config.twitter}`} />
-        )}
-
-        {socialDescription && (
-          <>
-            <meta name='description' content={socialDescription} />
-            <meta property='og:description' content={socialDescription} />
-            <meta name='twitter:description' content={socialDescription} />
-          </>
-        )}
-
-        {socialImage ? (
-          <>
-            <meta name='twitter:card' content='summary_large_image' />
-            <meta name='twitter:image' content={socialImage} />
-            <meta property='og:image' content={socialImage} />
-          </>
-        ) : (
-          <meta name='twitter:card' content='summary' />
-        )}
-
-        {canonicalPageUrl && (
-          <>
-            <link rel='canonical' href={canonicalPageUrl} />
-            <meta property='og:url' content={canonicalPageUrl} />
-            <meta property='twitter:url' content={canonicalPageUrl} />
-          </>
-        )}
-
-        <title>{title}</title>
-      </Head>
 
       <CustomFont site={site} />
 
@@ -276,14 +238,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }
       />
 
-<<<<<<< HEAD
-     
-
-      <CustomHtml site={site} />
-    </Twitter.Provider>
-=======
-      <GitHubShareButton />
+      
     </TwitterContextProvider>
->>>>>>> ba0c6f055aaa1553b5187ea98800a965ee998c93
   )
 }
