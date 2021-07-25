1ff86784629b1c030e1b6fdab33d14b5d49c6f5c
summerlovelemon <ybj8763@gmail.com>
2021-07-25 14:55:37 +0800

    web-font

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -23,6 +23,16 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
 
       <meta name='theme-color' content='#EB625A' />
       <meta property='og:type' content='website' />
+
+      <script type="text/javascript" src="http://cdn.repository.webfont.com/wwwroot/js/wf/youziku.api.min.js"></script>
+      <script type="text/javascript">
+        $webfont.load("body", "75a63f6c2f6d4bb7aee6902ac050dec2", "SourceHanS-S_B");
+        $webfont.load("notion-h-title", "57f09dabc1804f9b84b599c9b079f4f7", "Source-Han-Light")
+        $webfont.load("notion-callout-text", "4b16fb28383b409090d0b09c84c8c8d9", "JetLinkMediumMing");
+        /*$webfont.load("notion-callout-text", "75a63f6c2f6d4bb7aee6902ac050dec2", "SourceHanS-S_B");*/
+        /*．．．*/
+        $webfont.draw();
+      </script>
     </Head>
   )
 }
